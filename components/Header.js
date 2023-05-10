501f5ab5f8eb66424eb57552d31bba89e4e50ad0
unknown <aso.mansoury@gmail.com>
2023-05-10 18:09:48 +0430

    style the header section

--- components/Header.js
+++ components/Header.js
@@ -4,14 +4,15 @@ import {HomeIcon,UserIcon,PhoneIcon,InformationCircleIcon} from '@heroicons/reac
 
 export default function Header() {
   return (
-    <div>
-        <div className="">
+    <div className="bg-gray-700 text-gray-200 flex flex-col items-center p-6 select-none sm:flex-row justify-between">
+        <div className="flex flex-row ">
           <HeaderIcon Icon={HomeIcon} title="HOME"></HeaderIcon>
           <HeaderIcon Icon={UserIcon} title="ACCOUNT"></HeaderIcon>
           <HeaderIcon Icon={PhoneIcon} title="CONTACT"></HeaderIcon>
           <HeaderIcon Icon={InformationCircleIcon} title="ABOUNT"></HeaderIcon>
         </div>
         <Image 
+          className="cursor-pointer active:brightness-110"
           width={100} 
           height={100}
           src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/IMDB_Logo_2016.svg/575px-IMDB_Logo_2016.svg.png?20200406194337"
--- components/HeaderIcon.js
+++ components/HeaderIcon.js
@@ -1,8 +1,8 @@
 export default function HeaderIcon({Icon,title}) {
   return (
-    <div>
+    <div className="mx-4 flex flex-col hover:cursor-pointer hover:text-white active:text-red-400 lg:mx-6">
         <Icon className="h-8"></Icon>
-        <p>{title}</p>
+        <p className="my-2">{title}</p>
     </div>
   )
 }
